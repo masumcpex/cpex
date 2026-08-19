168f616f08ca7572b6097fdecd41a99de130e8b7
masumcpex <masumcpex@gmail.com>
2026-08-20 00:48:07 +0800

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zuzspxigzkatahmtopxa.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || 'Sb_publishable_MdePy9Or5nOyV-e7y6Cemg_PqOTOQ8Z'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
